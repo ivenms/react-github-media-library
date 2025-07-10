3f0f3d12d040e81d1cbf5f9f30a1112c8b8316a7
Iven Simon <ivenms@gmail.com>
2025-07-11 01:38:28 +0400

    Updated the category dropdown to avoid rendering if only one category of media

--- src/components/SearchAndFilter.tsx
+++ src/components/SearchAndFilter.tsx
@@ -31,28 +31,29 @@ export const SearchAndFilter: React.FC<SearchAndFilterProps> = ({
           onChange={(e) => onSearchChange(e.target.value)}
         />
       </div>
-      
-      <div className="rgml-relative">
-        <Filter className={`rgml-absolute rgml-left-3 rgml-top-1/2 rgml-transform -rgml-translate-y-1/2 rgml-w-5 rgml-h-5 ${
-          isDark ? 'rgml-text-gray-400' : 'rgml-text-gray-500'
-        }`} />
-        <select
-          value={selectedCategory}
-          onChange={(e) => onCategoryChange(e.target.value)}
-          className={`rgml-pl-10 rgml-pr-8 rgml-py-3 rgml-rounded-xl rgml-border-2 rgml-transition-all rgml-duration-200 focus:rgml-outline-none focus:rgml-ring-2 focus:rgml-ring-blue-500/20 rgml-min-w-[160px] ${
-            isDark 
-              ? 'rgml-bg-gray-800 rgml-border-gray-700 rgml-text-white focus:rgml-border-blue-500' 
-              : 'rgml-bg-white rgml-border-gray-200 rgml-text-gray-900 focus:rgml-border-blue-500'
-          }`}
-        >
-          <option value="All">All Categories</option>
-          {categories.map(category => (
-            <option key={category} value={category}>
-              {category}
-            </option>
-          ))}
-        </select>
-      </div>
+      {categories.length > 1 && (
+        <div className="rgml-relative">
+          <Filter className={`rgml-absolute rgml-left-3 rgml-top-1/2 rgml-transform -rgml-translate-y-1/2 rgml-w-5 rgml-h-5 ${
+            isDark ? 'rgml-text-gray-400' : 'rgml-text-gray-500'
+          }`} />
+          <select
+            value={selectedCategory}
+            onChange={(e) => onCategoryChange(e.target.value)}
+            className={`rgml-pl-10 rgml-pr-8 rgml-py-3 rgml-rounded-xl rgml-border-2 rgml-transition-all rgml-duration-200 focus:rgml-outline-none focus:rgml-ring-2 focus:rgml-ring-blue-500/20 rgml-min-w-[160px] ${
+              isDark 
+                ? 'rgml-bg-gray-800 rgml-border-gray-700 rgml-text-white focus:rgml-border-blue-500' 
+                : 'rgml-bg-white rgml-border-gray-200 rgml-text-gray-900 focus:rgml-border-blue-500'
+            }`}
+          >
+            <option value="All">All Categories</option>
+            {categories.map(category => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
     </div>
   );
 };
