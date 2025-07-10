24d0204ebd8b5e54488aecdfd5fc2b7b49617173
Iven Simon <ivenms@gmail.com>
2025-07-11 01:52:29 +0400

    Updated the select dropdown to render UI uniformally in all devices

--- src/components/SearchAndFilter.tsx
+++ src/components/SearchAndFilter.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Search, Filter } from 'lucide-react';
+import { Search, Filter, ChevronDown } from 'lucide-react';
 import { isDarkTheme } from '../utils';
 import { SearchAndFilterProps } from '../types';
 
@@ -32,14 +32,14 @@ export const SearchAndFilter: React.FC<SearchAndFilterProps> = ({
         />
       </div>
       {categories.length > 1 && (
-        <div className="rgml-relative">
+        <div className="rgml-relative rgml-min-w-[160px]">
           <Filter className={`rgml-absolute rgml-left-3 rgml-top-1/2 rgml-transform -rgml-translate-y-1/2 rgml-w-5 rgml-h-5 ${
             isDark ? 'rgml-text-gray-400' : 'rgml-text-gray-500'
           }`} />
           <select
             value={selectedCategory}
             onChange={(e) => onCategoryChange(e.target.value)}
-            className={`rgml-pl-10 rgml-pr-8 rgml-py-3 rgml-rounded-xl rgml-border-2 rgml-transition-all rgml-duration-200 focus:rgml-outline-none focus:rgml-ring-2 focus:rgml-ring-blue-500/20 rgml-min-w-[160px] ${
+            className={`rgml-appearance-none rgml-w-full rgml-pl-10 rgml-pr-8 rgml-py-3 rgml-rounded-xl rgml-border-2 rgml-transition-all rgml-duration-200 focus:rgml-outline-none focus:rgml-ring-2 focus:rgml-ring-blue-500/20 ${
               isDark 
                 ? 'rgml-bg-gray-800 rgml-border-gray-700 rgml-text-white focus:rgml-border-blue-500' 
                 : 'rgml-bg-white rgml-border-gray-200 rgml-text-gray-900 focus:rgml-border-blue-500'
@@ -52,6 +52,9 @@ export const SearchAndFilter: React.FC<SearchAndFilterProps> = ({
               </option>
             ))}
           </select>
+          <ChevronDown className={`rgml-absolute rgml-right-3 rgml-top-1/2 rgml-transform -rgml-translate-y-1/2 rgml-w-5 rgml-h-5 rgml-pointer-events-none ${
+            isDark ? 'rgml-text-gray-400' : 'rgml-text-gray-500'
+          }`} />
         </div>
       )}
     </div>
